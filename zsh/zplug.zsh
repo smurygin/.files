2ea064c35dbc72c96e5f8511ba39401322d138b7
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-09-08 01:10:52 +0300

    Update zplug.zsh

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -5,11 +5,6 @@ ZPLUG_HOME=$(brew --prefix)/opt/zplug
 
 [ -f $ZPLUG_HOME/init.zsh ] && source $ZPLUG_HOME/init.zsh
 
-zplug "plugins/git", from:oh-my-zsh
-zplug "plugins/docker", from:oh-my-zsh
-zplug "plugins/docker-compose", from:oh-my-zsh
-zplug "zsh-users/zsh-autosuggestions"
-zplug "zdharma/fast-syntax-highlighting"
 zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
 
 # Install plugins if there are plugins that have not been installed
