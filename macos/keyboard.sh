9c00597ba844432776a853c235c91b3622185831
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-09-09 00:08:59 +0300

    Update keyboard.sh

--- macos/keyboard.sh
+++ macos/keyboard.sh
@@ -2,24 +2,24 @@
 # Keyboard                                                                    #
 ###############################################################################
 
-# Increase key repeat rate
-defaults write -g InitialKeyRepeat -int 10
+# Increase key repeat rate !
+defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 
-# Disable automatic capitalization as it’s annoying when typing code
+# Disable automatic capitalization as it’s annoying when typing code !
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
-# Disable smart dashes as they’re annoying when typing code
+# Disable smart dashes as they’re annoying when typing code !
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
-# Disable automatic period substitution as it’s annoying when typing code
+# Disable automatic period substitution as it’s annoying when typing code !
 defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 
-# Disable smart quotes as they’re annoying when typing code
+# Disable smart quotes as they’re annoying when typing code !
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
-# Automatically illuminate built-in MacBook keyboard in low light
+# Automatically illuminate built-in MacBook keyboard in low light !
 defaults write com.apple.BezelServices kDim -bool true
 
-# Turn off keyboard illumination when computer is not used for 5 minutes
+# Turn off keyboard illumination when computer is not used for 5 minutes !
 defaults write com.apple.BezelServices kDimTime -int 300
