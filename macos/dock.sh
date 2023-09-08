248ce6f5f21cc23a37b14ed012ef4fa0572f6f6e
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-09-09 00:04:30 +0300

    Update dock.sh

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,20 +2,20 @@
 # Dock                                                                        #
 ###############################################################################
 
-# Show indicator lights for open applications in the Dock
+# Show indicator lights for open applications in the Dock !
 defaults write com.apple.dock show-process-indicators -bool true
 
-# Disable Dashboard
+# Disable Dashboard !
 defaults write com.apple.dashboard mcx-disabled -bool true
 
-# Don’t show Dashboard as a Space
+# Don’t show Dashboard as a Space !
 defaults write com.apple.dock dashboard-in-overlay -bool true
 
-# Don’t automatically rearrange Spaces based on most recent use
+# Don’t automatically rearrange Spaces based on most recent use !
 defaults write com.apple.dock mru-spaces -bool false
 
-# Don’t show recent applications in Dock
+# Don’t show recent applications in Dock !
 defaults write com.apple.dock show-recents -bool false
 
-# Don’t animate opening applications from the Dock
+# Don’t animate opening applications from the Dock !
 defaults write com.apple.dock launchanim -bool false
