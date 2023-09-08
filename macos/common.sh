1e99b9d24e955045a42dacd413a04678d97eacf0
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-09-09 00:03:34 +0300

    Update common.sh

--- macos/common.sh
+++ macos/common.sh
@@ -2,40 +2,29 @@
 # UI / UX                                                                     #
 ###############################################################################
 
-# Adjust toolbar title rollover delay
+# Adjust toolbar title rollover delay !
 defaults write NSGlobalDomain NSToolbarTitleViewRolloverDelay -float 0
 
-# Set sidebar icon size to Small
+# Set sidebar icon size to Small !
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
 
-# Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-# Expand print panel by default
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-# Automatically quit printer app once the print jobs complete
+# Automatically quit printer app once the print jobs complete !
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# Restart automatically if the computer freezes
+# Restart automatically if the computer freezes !
 sudo systemsetup -setrestartfreeze on
 
-# Show battery percentage
-defaults write com.apple.menuextra.battery ShowPercent YES
+# Show battery percentage !
+# defaults write com.apple.menuextra.battery ShowPercent YES
 
-# Disable flash Clock indicator
+# Disable flash Clock indicator !
 defaults write com.apple.menuextra.clock FlashDateSeparators -bool false
 
-# Action on double-clicking window
-defaults write -g AppleActionOnDoubleClick -string "Maximize"
-
-# Stop iTunes from responding to the keyboard media keys
+# Stop iTunes from responding to the keyboard media keys !
 launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
 
-# Save screenshots to the desktop
+# Save screenshots to the desktop !
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
-# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF) !
 defaults write com.apple.screencapture type -string "png"
