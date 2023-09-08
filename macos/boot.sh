00503872123019aba0fe5866de673d1f3563ce0d
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-09-09 00:10:20 +0300

    Update boot.sh

--- macos/boot.sh
+++ macos/boot.sh
@@ -2,15 +2,12 @@
 # Boot                                                                        #
 ###############################################################################
 
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
-
-# Disable audio feedback when volume is changed
+# Disable audio feedback when volume is changed !
 defaults write com.apple.sound.beep.feedback -bool false
 
-# Show language menu in the top right corner of the boot screen
+# Show language menu in the top right corner of the boot screen !
 sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
 
-# Require password immediately after sleep or screen saver begins
+# Require password immediately after sleep or screen saver begins !
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
