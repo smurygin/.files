ab843421b894a1a091b3d8e56ba0cfaa3c1c78df
Dmitry Smurygin <d.a.smurygin@gmail.com>
2023-09-09 00:05:29 +0300

    Update finder.sh

--- macos/finder.sh
+++ macos/finder.sh
@@ -2,26 +2,23 @@
 # Finder                                                                      #
 ###############################################################################
 
-# Show all hidden Files and Folders
+# Show all hidden Files and Folders !
 defaults write com.apple.Finder AppleShowAllFiles -bool true
 
-# Show all filename extensions
+# Show all filename extensions !
 defaults write com.apple.Finder AppleShowAllExtensions -bool true
 
-# Avoid creating .DS_Store files on network or USB volumes
+# Avoid creating .DS_Store files on network or USB volumes !
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# Keep folders on top when sorting by name
+# Keep folders on top when sorting by name !
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
-# Finder: allow text selection in Quick Look
+# Finder: allow text selection in Quick Look !
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
-# Disable the warning when changing a file extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-# Disable the warning when changing a file extension
+# Disable the warning when changing a file extension !
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 # When performing a search, search the current folder by default
@@ -31,11 +28,11 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 # Previous Scope : `SCsp`
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Finder: disable window animations and Get Info animations
+# Finder: disable window animations and Get Info animations !
 defaults write com.apple.finder DisableAllAnimations -bool true
 
-# Show the ~/Library folder
+# Show the ~/Library folder !
 chflags nohidden ~/Library
 
-# Show the /Volumes folder
+# Show the /Volumes folder !
 sudo chflags nohidden /Volumes
